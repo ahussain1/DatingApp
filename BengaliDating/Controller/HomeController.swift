98bef616405a6d780634d504d9b28f9cb186a0e6
Arif Hussain <hussain.arif33@gmail.com>
2022-11-12 11:00:16 -0500

    First commit

--- BengaliDating/Controller/HomeController.swift
+++ BengaliDating/Controller/HomeController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeController.swift
 //  BengaliDating
 //
 //  Created by Arif  on 11/12/22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
-
 }
-
